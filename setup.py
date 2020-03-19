d79e71f31a89f2aa3ce8824f49e7b380a5681907
Patrick Cullen <patrickbcullen@gmail.com>
2020-03-19 16:57:11 -0400

    updated to python3 and bumped version to 0.4.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import warnings
 
 setup(
     name='cloud-compose-ecs',
-    version='0.1.7',
+    version='0.4.0',
     description='ECS plugin for cloud-compose',
     packages=find_packages(),
     include_package_data=True,
